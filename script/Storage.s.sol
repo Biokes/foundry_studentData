8315ac4317d9f292ea1097e867d2048c70e04703
Biokes <106444765+Biokes@users.noreply.github.com>
2025-07-25 00:42:28 +0100

    [main] (UPDATE): update dotenv

--- script/Storage.s.sol
+++ script/Storage.s.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+
 import {StudentData} from "../src/StudentData.sol";
 
 contract CounterScript is Script {
